843b9be89507786366f59ef29d1c4c181568b69e
mhsmith01 <MHSmith01@gmail.com>
2017-02-28 14:30:23 -0800

    Initial Project Checkin
    Basic Unity3d starter with some Hexagon grid code

---
+++ Assets/HexCell.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+    private HexGrid _grid;
+
+    public int Elevation;
+
+    public int X;
+    public int Y;
+
+    public HexCell(HexGrid grid, int x, int y)
+    {
+        this._grid = grid;
+        this.X = x;
+        this.Y = y;
+
+        this.Elevation = 0;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public Vector3 ComputeCellCenter()
+    {
+        Vector3 position = _grid.Origin;
+        position.x += _grid.HexWidth / 2 * X;
+        position.y += _grid.StepHeight * Elevation;
+        position.z += Y * _grid.HexHeight * 2;
+        if (X % 2 == 1)
+        {
+            //odd row
+            position.z += _grid.HexHeight + _grid.HexHeight / 2;
+        }
+
+        return position;
+    }
+}
---
+++ Assets/HexGrid.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    private bool dirtySize;
+    private int width;
+    private int height;
+
+    public int GridWidth
+    {
+        get { return width; }
+        set
+        {
+            if (width != value)
+            {
+                dirtySize = true;
+                width = value;
+            }
+        }
+    }
+
+
+    public int GridHeight
+    {
+        get { return height; }
+        set
+        {
+            if (height != value)
+            {
+                dirtySize = true;
+                height = value;
+            }
+        }
+    }
+
+    private float hexHeight;
+    private float hexWidth;
+
+    public float HexHeight
+    {
+        get { return hexHeight; }
+        set
+        {
+            this.hexHeight = value;
+            this.hexWidth = (value * Mathf.Sqrt(3)) / 2;
+        }
+    }
+
+    public float HexWidth
+    {
+        get { return this.hexWidth; }
+    }
+
+    public float StepHeight;
+
+    public Vector3 Origin;
+
+    public HexCell[,] Cells;
+
+    public bool Wrap;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Origin = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateGridArea()
+    {
+        if (this.dirtySize)
+        {
+            this.dirtySize = false;
+
+            HexCell[,] next = new HexCell[GridWidth, GridHeight];
+            for (int x = 0; x < Cells.GetLength(0); x++)
+            {
+                for (int y = 0; y < Cells.GetLength(1); y++)
+                {
+                    next[x, y] = Cells[x, y];
+                }
+            }
+
+            for (int x = 0; x < GridWidth; x++)
+            {
+                for (int y = 0; y < GridHeight; y++)
+                {
+                    if (next[x, y] == null)
+                    {
+                        next[x,y] = new HexCell(this, x, y);
+                    }
+                }
+            }
+        }
+    }
+
+}
